2e30de0ad314828e0f748379157c8c37539e839f
Sillinous <travisgray85@gmail.com>
2026-09-07 22:05:19 -0500

    Export scan ingestion and processing APIs

--- services/vision/__init__.py
+++ services/vision/__init__.py
@@ -1,7 +1,9 @@
+from .ingestion import MediaStore, ScanIngestionService, StoredMedia
 from .media import FrameExtractor, ImageFrameExtractor, MediaAsset, OpenCVFrameExtractor, default_extractor
 from .models import BoundingBox, Frame, MediaType, PartCandidate, PieceObservation, ScanResult
 from .pipeline import EmptyVisionEngine, ScanPipeline, ScanPipelineResult, VisionEngine
 from .scan_jobs import InMemoryScanJobStore, ScanJob, ScanStatus
+from .scan_processor import ProcessedScan, ScanProcessor
 
 __all__ = [
     "BoundingBox",
@@ -11,15 +13,20 @@ __all__ = [
     "ImageFrameExtractor",
     "InMemoryScanJobStore",
     "MediaAsset",
+    "MediaStore",
     "MediaType",
     "OpenCVFrameExtractor",
     "PartCandidate",
     "PieceObservation",
+    "ProcessedScan",
+    "ScanIngestionService",
     "ScanJob",
+    "ScanProcessor",
     "ScanPipeline",
     "ScanPipelineResult",
     "ScanResult",
     "ScanStatus",
+    "StoredMedia",
     "VisionEngine",
     "default_extractor",
 ]
