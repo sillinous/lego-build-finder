e7a832fbf69d16f419c06d1af6cc0b3d592049c0
Sillinous <travisgray85@gmail.com>
2026-09-07 21:45:34 -0500

    Expose LEGO domain package

---
+++ packages/domain/__init__.py
@@ -0,0 +1,23 @@
+from .lego_domain import (
+    Inventory,
+    InventoryKey,
+    LegoSet,
+    MatchMode,
+    MatchResult,
+    Piece,
+    SetRequirement,
+    match_inventory,
+    rank_matches,
+)
+
+__all__ = [
+    "Inventory",
+    "InventoryKey",
+    "LegoSet",
+    "MatchMode",
+    "MatchResult",
+    "Piece",
+    "SetRequirement",
+    "match_inventory",
+    "rank_matches",
+]
