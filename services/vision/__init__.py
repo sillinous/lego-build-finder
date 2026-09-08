b58de06f7a49cee242072e80cb54e1f114d608ac
Sillinous <travisgray85@gmail.com>
2026-09-07 22:08:41 -0500

    Export vision engine contracts

--- services/vision/__init__.py
+++ services/vision/__init__.py
@@ -1,3 +1,7 @@
+from .classifier import EmptyPartClassifier, PartClassifier
+from .detector import Detection, EmptyPieceDetector, PieceDetector
+from .engine import LegoVisionEngine
+from .image_loader import ImageLoader, OpenCVImageLoader
 from .ingestion import MediaStore, ScanIngestionService, StoredMedia
 from .media import FrameExtractor, ImageFrameExtractor, MediaAsset, OpenCVFrameExtractor, default_extractor
 from .models import BoundingBox, Frame, MediaType, PartCandidate, PieceObservation, ScanResult
@@ -7,16 +11,24 @@ from .scan_processor import ProcessedScan, ScanProcessor
 
 __all__ = [
     "BoundingBox",
+    "Detection",
+    "EmptyPartClassifier",
+    "EmptyPieceDetector",
     "EmptyVisionEngine",
     "Frame",
     "FrameExtractor",
     "ImageFrameExtractor",
+    "ImageLoader",
     "InMemoryScanJobStore",
+    "LegoVisionEngine",
     "MediaAsset",
     "MediaStore",
     "MediaType",
     "OpenCVFrameExtractor",
+    "OpenCVImageLoader",
     "PartCandidate",
+    "PartClassifier",
+    "PieceDetector",
     "PieceObservation",
     "ProcessedScan",
     "ScanIngestionService",
