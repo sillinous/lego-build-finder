6f75c97829a6fe2f37746d76a1322e2583a8b2d1
Sillinous <travisgray85@gmail.com>
2026-09-07 22:02:38 -0500

    Export scan media and pipeline components

--- services/vision/__init__.py
+++ services/vision/__init__.py
@@ -1,10 +1,25 @@
+from .media import FrameExtractor, ImageFrameExtractor, MediaAsset, OpenCVFrameExtractor, default_extractor
 from .models import BoundingBox, Frame, MediaType, PartCandidate, PieceObservation, ScanResult
+from .pipeline import EmptyVisionEngine, ScanPipeline, ScanPipelineResult, VisionEngine
+from .scan_jobs import InMemoryScanJobStore, ScanJob, ScanStatus
 
 __all__ = [
     "BoundingBox",
+    "EmptyVisionEngine",
     "Frame",
+    "FrameExtractor",
+    "ImageFrameExtractor",
+    "InMemoryScanJobStore",
+    "MediaAsset",
     "MediaType",
+    "OpenCVFrameExtractor",
     "PartCandidate",
     "PieceObservation",
+    "ScanJob",
+    "ScanPipeline",
+    "ScanPipelineResult",
     "ScanResult",
+    "ScanStatus",
+    "VisionEngine",
+    "default_extractor",
 ]
